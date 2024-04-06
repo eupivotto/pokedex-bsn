46579e07db66938bb79e5f3bc03a16c125819ee5
eupivotto <agenciadigi62@gmail.com>
2024-04-06 01:55:38 -0300

    Initial project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,20 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'category',
+    loadComponent: () => import('./modules/pages/category/category.page').then( m => m.CategoryPage)
+  },
+  {
+    path: 'pokemon-details',
+    loadComponent: () => import('./modules/pages/pokemon-details/pokemon-details.page').then( m => m.PokemonDetailsPage)
+  },
+  {
+    path: 'favorites',
+    loadComponent: () => import('./modules/pages/favorites/favorites.page').then( m => m.FavoritesPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./modules/pages/home/home.page').then( m => m.HomePage)
+  },
 ];
